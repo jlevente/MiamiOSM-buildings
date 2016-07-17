7b60392a81dd7f8c23ca982cc784d0eef10d1644
Levente Juhasz <jlevente89@gmail.com>
2016-07-17 14:53:29 -0400

    dummy status messages when importing shapefiles

--- data_prep/import_shapefiles.sh
+++ data_prep/import_shapefiles.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+echo 'Importing Large Buildings dataset...'
 ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres password=postgres dbname=osmbuildings_miami port=5432" -lco GEOMETRY_NAME=geom $1"/Large_Buildings_2013.shp"
 
+echo 'Importing Culture Venue dataset...'
 ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres password=postgres dbname=osmbuildings_miami port=5432" -lco GEOMETRY_NAME=geom $1"/Culture_Venue.shp"
 
+echo 'Importing Address dataset...'
 ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres password=postgres dbname=osmbuildings_miami port=5432" -lco GEOMETRY_NAME=geom $1"/Address.shp"
 
+echo 'Importing County Library dataset...'
 ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres password=postgres dbname=osmbuildings_miami port=5432" -lco GEOMETRY_NAME=geom $1"/County_Library.shp"
 
+echo 'Importing College dataset...'
 ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres password=postgres dbname=osmbuildings_miami port=5432" -lco GEOMETRY_NAME=geom $1"/College.shp"
