20df0a92e94b9e45f69edaeb11086b1f6377796b
Levente Juhasz <jlevente89@gmail.com>
2016-07-20 13:10:23 -0400

    generate osm files for special cases

--- data_conversion/generate_osm_files.sh
+++ data_conversion/generate_osm_files.sh
@@ -29,10 +29,11 @@ fi
 if [ "$1" == 'review' ]; then
   echo "Generating osm files based on Block Groups..."
   curr_dir=`pwd`;
+  mkdir -p blocks
+  out_folder='blocks'
+
   while IFS='' read -r objectid; do
     # echo  "$objectid"
-    mkdir -p blocks
-    out_folder='blocks'
     output_name=$objectid"_building.osm"
     translation="mia_building_trans.py"
 
@@ -63,5 +64,61 @@ if [ "$1" == 'review' ]; then
     ### TODO: get those outside of block groups
 
   done < $curr_dir/"data_conversion/block_objectids.csv"
+
+  echo '\nGenerating extra osm files for special cases...\n'
+  # Get buildings outside blocks
+
+  output_name="building_outside.osm"
+  translation="mia_building_trans.py"
+
+  sql="SELECT b.height, b.objectid, b.geom from buildings_overlap b, (select st_union(geom) as geom from block_groups_2010) block where not st_intersects(b.geom, block.geom)"
+  python $ogr2osm_dir/ogr2osm.py "$dsn" -t $translation -f -o $out_folder"/"$output_name --sql "$sql" --id="-1"
+
+  # Get address
+  translation="mia_address_trans.py"
+  output_name="address_outside.osm"
+
+  sql="SELECT a.objectid, zip, mailing_mu as city, pre_dir, st_name, st_type, suf_dir, hse_num as house_num, a.geom from address a,
+      (SELECT b.geom from buildings_overlap b, (select st_union(geom) as geom from block_groups_2010) block where
+      not st_intersects(b.geom,block.geom)) x where st_within(a.geom, x.geom)"
+  python $ogr2osm_dir/ogr2osm.py "$dsn" -t $translation -f -o $out_folder"/"$output_name --sql "$sql" --id="-20001"
+
+  # Fake version, timestamps
+  osmconvert $out_folder"/building_outside.osm" --fake-author -o=$out_folder"/building_outside_fake.osm"
+  osmconvert $out_folder"/address_outside.osm" --fake-author -o=$out_folder"/address_outside_fake.osm"
+
+  # Sort and merge
+  osmosis --read-xml $out_folder"/address_outside_fake.osm" --sort type="TypeThenId" --write-xml $out_folder"/address_outside_sort.osm"
+  osmosis --read-xml $out_folder"/building_outside_fake.osm" --sort type="TypeThenId" --write-xml $out_folder"/building_outside_sort.osm"
+  osmosis --read-xml $out_folder"/building_outside_sort.osm" --read-xml $out_folder"/address_outside_sort.osm" --merge --write-xml $out_folder"/outside.osm"
+
+  # Get buildings overlapping with block boundaries (i.e. share space with 2 blocks)
+
+  output_name="building_border.osm"
+  translation="mia_building_trans.py"
+
+  sql="SELECT b.height, b.objectid, b.geom from buildings_overlap b, (select geom as geom from block_groups_2010) block where st_overlaps(b.geom, block.geom) and not st_within(b.geom, block.geom)"
+  python $ogr2osm_dir/ogr2osm.py "$dsn" -t $translation -f -o $out_folder"/"$output_name --sql "$sql" --id="-1"
+
+  # Get address
+  translation="mia_address_trans.py"
+  output_name="address_border.osm"
+
+  sql="SELECT a.objectid, zip, mailing_mu as city, pre_dir, st_name, st_type, suf_dir, hse_num as house_num, a.geom from address a,
+      (SELECT b.geom from buildings_overlap b, (select geom as geom from block_groups_2010) block where
+       st_overlaps(b.geom, block.geom) and not st_within(b.geom, block.geom)) x where st_within(a.geom, x.geom)"
+  python $ogr2osm_dir/ogr2osm.py "$dsn" -t $translation -f -o $out_folder"/"$output_name --sql "$sql" --id="-20001"
+
+  # Fake version, timestamps
+  osmconvert $out_folder"/building_border.osm" --fake-author -o=$out_folder"/building_border_fake.osm"
+  osmconvert $out_folder"/address_border.osm" --fake-author -o=$out_folder"/address_border_fake.osm"
+
+  # Sort and merge
+  osmosis --read-xml $out_folder"/address_border_fake.osm" --sort type="TypeThenId" --write-xml $out_folder"/address_border_sort.osm"
+  osmosis --read-xml $out_folder"/building_border_fake.osm" --sort type="TypeThenId" --write-xml $out_folder"/building_border_sort.osm"
+  osmosis --read-xml $out_folder"/building_border_sort.osm" --read-xml $out_folder"/address_border_sort.osm" --merge --write-xml $out_folder"/border.osm"
+
+   # Cleanup
+    rm $out_folder"/building_border.osm" $out_folder"/building_border_fake.osm" $out_folder"/building_border_sort.osm" $out_folder"/address_border.osm" $out_folder"/address_border_fake.osm" $out_folder"/address_border_sort.osm" $out_folder"/building_outside.osm" $out_folder"/building_outside_fake.osm" $out_folder"/building_outside_sort.osm" $out_folder"/address_outside.osm" $out_folder"/address_outside_fake.osm" $out_folder"/address_outside_sort.osm"
 
 fi
